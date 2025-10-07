ba07418204efe2b72db3b92660a43401dce1f67f
BilalMP <peeroo270@gmail.com>
2025-10-07 16:12:36 +0400

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import AuthButton from "./auth-button";
+import ModeToggle from "./mode-toggle";
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center py-3 px-10 top-0 left-0 right-0 z-50 bg-slate-100">
+        <nav className="flex justify-between items-center py-3 px-10 top-0 left-0 right-0 z-50 bg-slate-100 dark:bg-slate-900 border-b border-slate-200 dark:border-slate-800">
             <Link
                 href="/"
-                className="text-2xl font-bold"
+                className="text-2xl font-bold text-slate-900 dark:text-slate-100"
             >
                 Better Auth
             </Link>
-            <AuthButton />
+            <div className="flex gap-5">
+                <ModeToggle />
+                <AuthButton />
+            </div>
         </nav>
     );
 };
