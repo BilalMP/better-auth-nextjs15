b5059c1dfeaa9b7a1f05b841a89bc3a6bd915b60
BilalMP <peeroo270@gmail.com>
2025-10-07 16:13:20 +0400

    Index Page

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ const Home = async () => {
         return (
             <div className="flex flex-1 justify-center items-center grow p-8">
                 <div className="flex flex-col items-center gap-4">
-                    <h1 className="text-7xl">Hello {session?.user?.name}</h1>
-                    <p>You are logged in as {session?.user?.email}</p>
+                    <h1 className="text-7xl text-slate-900 dark:text-slate-100">Hello {session?.user?.name}</h1>
+                    <p className="text-slate-600 dark:text-slate-400">You are logged in as {session?.user?.email}</p>
                 </div>
             </div>
         );
@@ -19,7 +19,7 @@ const Home = async () => {
 
     return (
         <div className="flex flex-1 justify-center items-center grow p-8 hscren">
-            <p className="text-7xl">You are not logged in ...</p>
+            <p className="text-7xl text-slate-900 dark:text-slate-100">You are not logged in ...</p>
         </div>
     );
 };
